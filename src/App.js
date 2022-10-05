3a2f09242d702b930610da39d2a2dda2fb1877a2
adilahamad786 <adilahamad.personally@gmail.com>
2022-10-05 12:08:30 +0530

    Rewirte code using 'async-await' syntex.

--- src/App.js
+++ src/App.js
@@ -6,22 +6,18 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films/")
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      const transformedMoviesDate = data.results.map(movieData => {
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+    const transformedMoviesDate = data.results.map(movieData => {
         return {
           id : movieData.episode_id,
           title : movieData.title,
           openingText : movieData.opening_crawl,
           releaseDate : movieData.release_date
         }
-      })
-      setMovies(transformedMoviesDate);
     })
+    setMovies(transformedMoviesDate);
   }
 
   return (
