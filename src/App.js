bfe96758b61aabf088100a3e788420a420ae66a8
adilahamad786 <adilahamad.personally@gmail.com>
2022-10-05 14:18:09 +0530

    Create AddMovie component form for adding movie.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useCallback, useState, useEffect } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from "./components/AddMovie";
 import './App.css';
 
 function App() {
@@ -39,6 +40,10 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  function addMovieHandler(movie) {
+    console.log(movie)
+  }
+
   let content = "Movies not found!";
 
   if (movies.length > 0) {
@@ -55,6 +60,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
